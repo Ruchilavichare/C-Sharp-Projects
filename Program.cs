e4c9d8db5ae6b95a087af2dd6905faa0ff3370b8
Ruchilavichare <ruchila.vichare@gmail.com>
2025-01-22 15:17:47 +0530

    brackets_balanced demo

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace BracketsBalanced
+{
+    class Program
+    {
+        static bool AreBracketsBalanced(string s)
+        {
+            string prev;
+            do
+            {
+                prev = s;
+                s = s.Replace("()", "").Replace("[]", "").Replace("{}", "");
+                Console.WriteLine(prev + "--" + s);
+            } while (s != prev);
+
+            return s.Length == 0;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(AreBracketsBalanced("{([])}"));  // True
+            //Console.WriteLine(AreBracketsBalanced("{{)()(}}"));  // False
+            //Console.WriteLine(AreBracketsBalanced("{([)]}"));  // False
+            //Console.WriteLine(AreBracketsBalanced("[({})]"));  // True
+            //Console.WriteLine(AreBracketsBalanced("((()))"));  // True
+            //Console.WriteLine(AreBracketsBalanced("((())"));   // False
+        }
+    }
+}
